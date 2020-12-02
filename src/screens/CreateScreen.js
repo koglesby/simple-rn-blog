e97691dccaf7ae51b5cdfc835863b213770bd5cd
koglesby <kevinjoglesby@gmail.com>
2020-12-02 14:42:05 -0800

    navigation on save

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -5,22 +5,30 @@ import { Context } from '../context/BlogContext';
 const CreateScreen = ({ navigation }) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const { addBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Text style={styles.label}>CreateScreen</Text>
+      <Text style={styles.label}>Enter Title:</Text>
       <TextInput
         style={styles.input}
         value={title}
         onChangeText={(text) => setTitle(text)}
       />
-      <Text style={styles.label}>Enter Content</Text>
+      <Text style={styles.label}>Enter Content:</Text>
       <TextInput
         style={styles.input}
         value={content}
         onChangeText={(text) => setContent(text)}
       />
-      <Button title="Add Blog Post" />
+      <Button
+        title="Add Blog Post"
+        onPress={() =>
+          addBlogPost(title, content, () => {
+            navigation.navigate('Index');
+          })
+        }
+      />
     </View>
   );
 };
