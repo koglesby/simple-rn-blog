60be5dca78d8708080cb510caa86a0a9fbe9a214
koglesby <kevinjoglesby@gmail.com>
2020-12-02 14:58:45 -0800

    add editscreen

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const EditScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>EditScreen - {navigation.getParam('id')}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default EditScreen;
